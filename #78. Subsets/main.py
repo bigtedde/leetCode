bb3421f0c37db8103f0e96de6d52f5cb710d5552
bigtedde <big.tedde@gmail.com>
2023-01-19 15:05:00 -0800

    #78. Subsets

--- #78. Subsets/main.py
+++ #78. Subsets/main.py
@@ -1,7 +1,35 @@
 # Ted Lawson
+from typing import List
+
 
 def main():
-    return
+    nums = [1, 2, 3, 4]
+    answer = Solution().subsets(nums)
+    print(f"final answer: {answer}")
+    return 
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        results = []
+        subset = []
+
+        def dfs(index: int):
+
+            if index == len(nums):
+                print(f"ADDING ENTRY TO FINAL RESULTS: {subset}")
+                results.append(subset[:]) # setset[:] == subset.copy(), more efficient
+                return
+
+            print(f"APPENDING {nums[index]} to {subset}")
+            subset.append(nums[index])
+            dfs(index + 1)
+
+            print(f"POPING {nums[index]} from {subset}")
+            subset.pop()
+            dfs(index + 1)
+
+        dfs(0)
+        return results
 
 
 if __name__ == "__main__":
